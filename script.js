d223b08a3153caa7d840cd3b0ab8ac589b393f09
PauloVtl <94381670+PauloVtl@users.noreply.github.com>
2021-12-01 12:12:23 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ function criarBG(){
 
 function criarCobrinha (){
     for(i = 0; i < snake.length; i++){
-        context.fillStyle = "green";
+        context.fillStyle = "yellow";
         context.fillRect(snake[i].x, snake[i].y, box, box);
     }
 }
